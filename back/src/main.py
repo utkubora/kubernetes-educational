d37996732ec5a781efb5677f87e75843af294858
Utku Bora <utkubora45@gmail.com>
2026-04-24 21:50:22 +0300

    code updated with bugfixes

--- back/src/main.py
+++ back/src/main.py
@@ -14,11 +14,10 @@ mongo_client = MongoDBClient()
 user_collection = mongo_client.get_collection("User")
 
 # Request/response model
-class Item(BaseModel):
-    id: int
+class User(BaseModel):
     name: str
-    price: float
-    in_stock: bool = True
+    role: str
+    skills: list = []
 
 
 @app.get("/")
@@ -29,18 +28,18 @@ def read_root():
     return {"message": "FastAPI is running"}
 
 
-@app.get("/items", response_model=List[Item])
+@app.get("/items", response_model=List[User])
 def get_items():
     """
     Return all items
     """
 
-    items_db = get_items(user_collection)
+    items_db = mongo_findAll(user_collection)
 
     return items_db
 
 
-@app.get("/items/{item_id}", response_model=Item)
+@app.get("/items/{item_id}", response_model=User)
 def get_item(item_id: int):
     """
     Return one item by ID
@@ -49,12 +48,12 @@ def get_item(item_id: int):
     #    if item.id == item_id:
     #        return item
         
-    items_db = get_items(user_collection)
+    items_db = mongo_findAll(user_collection)
 
     raise HTTPException(status_code=404, detail="Item not found")
 
 
-@app.post("/items", response_model=Item, status_code=201)
+@app.get("/add", response_model=User, status_code=201)
 def create_item():
     """
     Create a new item
@@ -63,12 +62,6 @@ def create_item():
     #for existing_item in items_db:
     #    if existing_item.id == item.id:
     #        raise HTTPException(status_code=400, detail="Item with this ID already exists")
-
-    item = {
-        "name": "Utku",
-        "role": "Engineer",
-        "skills": ["Python", "MongoDB", "DevOps"]
-    }
-
+    item = User("Utku","Engineer",["Python", "MongoDB", "DevOps"])
     mongo_insertOne(user_collection,item)
     return item
